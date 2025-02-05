14d84374bbdab9457e5b41e2054d13b893f92c26
NatyBlass <natypaz12345@gmail.com>
2025-02-05 14:58:09 -0300

    Migracion de archivos del tp anterior realizada

---
+++ Models/Presupuesto.cs
@@ -0,0 +1,50 @@
+public class Presupuesto
+{
+    private int idPresupuesto;
+    private string nombreDestinatario;
+    private DateTime fechaCreacion;
+    private List<PresupuestoDetalle> detalle;
+
+    public int IdPresupuesto { get => idPresupuesto; set => idPresupuesto = value; }
+    public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
+    public DateTime FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
+    public List<PresupuestoDetalle> Detalle { get => detalle; set => detalle = value; }
+
+    public Presupuesto(int idPresupuesto, string nombreDestinatario, DateTime fechaCreacion)
+    {
+        this.idPresupuesto = idPresupuesto;
+        this.nombreDestinatario = nombreDestinatario;
+        this.fechaCreacion = fechaCreacion;
+        this.detalle = new List<PresupuestoDetalle>();
+    }
+
+
+    public float MontoPresupuesto()
+    {
+        float total = 0;
+        foreach (var item in detalle)
+        {
+            total = total + item.Prod.Precio * item.Cantidad;
+        }
+
+        return total;
+    }
+
+    public float MontoPresupuestoConIVA()
+    {
+        float totalConIVA = MontoPresupuesto() + (MontoPresupuesto() * 21)/ 100;
+        return totalConIVA;
+    }
+
+    public int CantidadProductos()
+    {
+        int cantTotal = 0;
+        foreach (var item in detalle)
+        {
+            cantTotal = cantTotal + item.Cantidad;
+        }
+
+        return cantTotal;
+    }
+
+}
